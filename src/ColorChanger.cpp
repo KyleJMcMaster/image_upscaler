4a53a6410d22f5da160e3eb692dd8fd74e164654
KyleJMcMaster <kyle.mcmaster@rogers.com>
2024-01-12 21:01:18 -0500

    update main

--- src/ColorChanger.cpp
+++ src/ColorChanger.cpp
@@ -1,12 +1,20 @@
 #include "../headers/ColorChanger.h"
 
 
-void ColorChanger::set_color(Image* img, int channel, std::vector<int> new_channel){
-    //img->
-
+void ColorChanger::set_color(Image* img, int channel, std::vector<int> new_value){
+    std::vector<int> dims = img->get_size();
+    int size = dims[0] * dims[1];
+    new_value.resize(size, 0); //make sure sizes match
+    img->set_channel(channel, new_value);
 }
 
 void ColorChanger::shift_color(Image* img, int channel, int shift_amount){
-
-
+    int max_col = img->get_max_col();
+    std::vector<int> dims = img->get_size();
+    int size = dims[0] * dims[1];
+    std::vector<int> value = img->get_channels()[channel];
+    for (int i = 0; i < size; i++) {
+        int x = value[i]+shift_amount;
+        value[i] = (x>=max_col)*max_col + (x<max_col)*x; //limit to max_col without conditional
+    }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,17 @@
 #include "../headers/Image.h"
+#include "../headers/ColorChanger.h"
 #include <iostream>
 #include <string>
 #include <vector>
 
 int main()
 {
-
+    std::cout << "here-1";
     Image im1("resources/dog_water.ppm", true);
     im1.set_save_filepath("resources/dog_water_red.ppm");
-
-    std::vector<int> dim = im1.get_size();
-    std::vector<int> channel;
-    int length = dim[0]*dim[1];
-    for(int i = 0; i < length; i++){
-        channel.push_back(0);
-    }
-    im1.set_channel(0, channel);
+    std::cout << "here";
+    ColorChanger::shift_color(&im1, 0, 50);
+    std::cout << "here1";
     im1.save_image();
     return 0;
 }
